4e8e85f5d59b398c6c7cbaf7870c73e9a4e4088e
VA <54583234+vickyarisandy@users.noreply.github.com>
2022-11-17 01:06:30 +0800

    home 6

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project01/shared/theme.dart';
 
 class HomePage extends StatelessWidget{
   const HomePage({Key?key}): super(key:key);
@@ -6,7 +7,52 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-
+        backgroundColor: lightBackgroundColor,
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                  'assets/gambar/ic_overview.png',
+                  width: 20,
+                color: blueColor,
+                ),
+                label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/gambar/ic_history.png',
+                width: 20,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/gambar/ic_Statistik.png',
+                width: 20,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/gambar/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Overview',
+            ),
+          ],
+        ),
     );
   }
 }
--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -1,47 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:project01/shared/theme.dart';
+import 'package:project01/ui/widgets/buttons.dart';
 
 class SignInPage extends StatelessWidget{
   const SignInPage({Key? key}): super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(top: 120, left: 24, right: 24),
-          child: Center(
-            child: Column(
-              children: [
-                Text(
-                    'Welcome back',
-                  style: TextStyle(fontSize: 20),
-                ),
-                SizedBox(height: 20,),
-                Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        fillColor: Color(0xffF1F0F5),
-                        filled: false,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide(),
-                        ),
-                        labelText: 'nama *',
-                      ),
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+        body: ListView(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Lupa Password',
+                      style: blueTextStyle,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  CustomFilledButton(
+                      title: 'Sign in',
+                    onPressed: (){
+                      Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomFilledButton(
+                title: 'buat akun',
+              onPressed: () {
+                  Navigator.pushNamed(context, 'sign-up');
+              },
+            ),
+          ],
         ),
-      ),
     );
   }
 
