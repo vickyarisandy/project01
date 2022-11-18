591946a3b53824d5b29988c86fbfd5b26bc3893a
VA <54583234+vickyarisandy@users.noreply.github.com>
2022-11-19 03:51:15 +0800

    more

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -287,7 +287,12 @@ class HomePage extends StatelessWidget{
               ),HomeServiceItem(
                 iconUrl: 'assets/ic_more.png',
                 title: 'More',
-                onTap: (){},
+                onTap: (){
+                  showDialog(
+                    context: context,
+                    builder: (context) => const MoreDialog(),
+                  );
+                },
               ),
             ],
           ),
@@ -453,6 +458,29 @@ class HomePage extends StatelessWidget{
           ),
 
         ],
+      ),
+    );
+  }
+
+}
+
+
+class MoreDialog extends StatelessWidget{
+  const MoreDialog({Key?key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return AlertDialog(
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.zero,
+      alignment: Alignment.bottomCenter,
+      content: Container(
+        height: 326,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: lightBackgroundColor,
+        ),
       ),
     );
   }
